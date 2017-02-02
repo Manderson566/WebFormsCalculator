9d73eb79e8058d68e3bd95ead170219e57dec5db
Michael Anderson <Micmail566@gmail.com>
2017-02-02 02:24:33 -0500

    Add files via upload

---
+++ Web Forms Calculator/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+        <h1>Calculator</h1>
+
+        <h4>Enter your numbers and select an operator.</h4>
+         <h3><%=Equation %></h3>
+        <h3><%=Answer %></h3>
+        <br />
+        <form id="form1" runat="server">
+            <div class="form-group">
+                <input type="text" class="form-control" name="Number1" />
+                <br />
+                <select name="DropDown">
+                    <%
+                        foreach (var operation in Operators)
+                        { %>
+                    <option value="<%=operation %>"><%= operation %></option>
+                    <% } %>
+                </select>
+                <h4>Select Your Operator</h4>
+                <br />
+                <input type="text" class="form-control" name="Number2" />
+            </div>
+            <div>
+                <input type="submit" class="btn btn-primary" value="Submit" />
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ Web Forms Calculator/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string addition;
+    public string Equation;
+    public string Answer1;
+    public string Answer2;
+    public double Answer;
+    public double ans1;
+    public double ans2;
+    public List<string> Operators = new List<string>
+    {
+        "Addition +",
+        "Subtraction -",
+        "Multiplication *",
+        "Division /"
+    };
+    public string DropDown = "";
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+
+            Answer1 = Request.Form["Number1"];
+            Answer2 = Request.Form["Number2"];
+            DropDown = Request.Form["DropDown"];
+            while (Answer1 == "" || Answer2 == "")
+             Answer = 0;
+            string plus = "+";
+            string minus = "-";
+            string multiply = "*";
+            string divide = "/";
+            ans1 = double.Parse(Answer1);
+            ans2 = double.Parse(Answer2);
+            foreach (var operation in Operators)
+            {
+                if (Request.Form["DropDown"] == "Addition +")
+                {
+                    Answer = ans1 + ans2;
+                    Equation = Answer1 + plus + Answer2;
+                }
+                else if (Request.Form["DropDown"] == "Subtraction -")
+                {
+                    Answer = ans1 - ans2;
+                    Equation = Answer1 + minus + Answer2;
+                }
+                else if (Request.Form["DropDown"] == "Multiplication *")
+                {
+                    Answer = ans1 * ans2;
+                    Equation = Answer1 + multiply + Answer2;
+                }
+                else if (Request.Form["DropDown"] == "Division /")
+                {
+                    Answer = ans1 / ans2;
+                    Equation = Answer1 + divide + Answer2;
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
